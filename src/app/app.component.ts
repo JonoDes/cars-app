af9c771bce857f72988821dc0d8ec55021fa3423
JonoDes <rmn.hetzner@gmail.com>
2023-01-15 22:09:58 +0300

    preprod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,86 +1,75 @@
 import { Component, HostListener } from '@angular/core';
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   priceForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.pattern(/^[A-ZА-Я][a-zа-я]{2,}$/)]),
-    phone: new FormControl('', [Validators.required, Validators.pattern(/^\+*\d{11}$/)]),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[A-ZА-Я][a-zа-я]{2,}$/),
+    ]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\+*\d{10,13}$/),
+    ]),
     car: new FormControl('', Validators.required),
   });
 
-  carsData = [
-    {
-      image: "1.png",
-      name: "Lamborghini Huracan Spyder",
-      gear: "full",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "2.png",
-      name: "Chevrolet Corvette",
-      gear: "full",
-      engine: 6.2,
-      places: 2,
-    },
-    {
-      image: "3.png",
-      name: "Ferrari California",
-      gear: "full",
-      engine: 3.9,
-      places: 4,
-    },
-    {
-      image: "4.png",
-      name: "Lamborghini Urus",
-      gear: "full",
-      engine: 4.0,
-      places: 5,
-    },
-    {
-      image: "5.png",
-      name: "Audi R8",
-      gear: "full",
-      engine: 5.2,
-      places: 2,
-    },
-    {
-      image: "6.png",
-      name: "Chevrolet Camaro",
-      gear: "full",
-      engine: 2.0,
-      places: 4,
-    },
-  ];
+  carsData: any;
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.appService.getData().subscribe(carsData => this.carsData = carsData);
+  }
 
   goScroll(target: HTMLElement, car?: any) {
-    target.scrollIntoView({ behavior: "smooth" });
+    target.scrollIntoView({ behavior: 'smooth' });
     if (car) {
       this.priceForm.patchValue({ car: car.name });
     }
-  };
+  }
 
   trans: any;
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: MouseEvent) {
-    this.trans = { transform: 'translate3d(' + ((e.clientX * 0.4) / 8) + 'px,' + ((e.clientY * 0.4) / 8) + 'px,0px)' };
+    this.trans = {
+      transform:
+        'translate3d(' +
+        (e.clientX * 0.4) / 8 +
+        'px,' +
+        (e.clientY * 0.4) / 8 +
+        'px,0px)',
+    };
   }
 
   bgPos: any;
   @HostListener('document:scroll', ['$event'])
   onScroll() {
-    this.bgPos = { backgroundPositionX: '0' + (0.5 * window.scrollY) + 'px' };
+    this.bgPos = { backgroundPositionX: '0' + 0.5 * window.scrollY + 'px' };
   }
 
   onSubmit() {
     if (this.priceForm.valid) {
-      alert("Thank you for your request, we will contact you shortly");
-      this.priceForm.reset();
+      this.appService.sendQuery(this.priceForm.value).subscribe({
+        next: (response: any) => {
+          alert(response.message);
+          this.priceForm.reset();
+        },
+        error: (response) => {
+          alert(response.error.message);
+        },
+      });
     }
   }
 }
